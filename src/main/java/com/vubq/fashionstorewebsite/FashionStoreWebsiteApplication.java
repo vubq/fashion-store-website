c49e676dea5fe004ad2afcd9e634b9cae6268324
Quang Vũ <vubq242002@gmail.com>
2024-01-31 22:41:24 +0700

    Init project backend

---
+++ src/main/java/com/vubq/fashionstorewebsite/FashionStoreWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.vubq.fashionstorewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionStoreWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionStoreWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vubq/fashionstorewebsite/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vubq.fashionstorewebsite;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FashionStoreWebsiteApplication.class);
+	}
+
+}
